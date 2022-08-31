1d62083cf3e692eee4d2908df85ed10074ff320c
Ak Programz <112052705+ansarmonu@users.noreply.github.com>
2022-08-31 17:56:01 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -8,9 +8,9 @@ let animation=[
 	{text:"PRO",delay:1500},
 	{text:"PROG",delay:2000},
 	{text:"PROGR",delay:2500},
-	{text:"PROGRA.",delay:3000},
-	{text:"PROGRAM.",delay:3500},
-	{text:"PROGRAMZ.",delay:4000}
+	{text:"PROGRA",delay:3000},
+	{text:"PROGRAM",delay:3500},
+	{text:"PROGRAMZ",delay:4000}
 ]
 
 let caller=(text,delay)=>{
